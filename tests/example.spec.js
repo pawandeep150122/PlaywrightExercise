4afcb7dc9cebc5c52a21c7b208be3926935e2513
pawansingh79 <pawandeepit@gmail.com>
2025-07-31 16:48:04 +0100

    Initial repository

---
+++ tests/example.spec.js
@@ -0,0 +1,20 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://www.automationexercise.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Automation Exercise/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://www.automationexercise.com/');
+  /*
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  */
+});
